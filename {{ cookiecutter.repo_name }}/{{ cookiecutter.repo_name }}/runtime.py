eda6daf55f68f549fe4234355a4552a75ec76e67
Alejandro Saucedo <axsauze@gmail.com>
2022-06-03 16:04:24 +0100

    Added base cookiecutter runtime

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/runtime.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/runtime.py
@@ -1,94 +1,59 @@
 import json
 import asyncio
+import pickle
 from mlserver.model import MLModel
 from mlserver.settings import ModelSettings
+from fastapi import status
+from mlserver.utils import get_model_uri
+from mlserver.errors import InvalidModelURI, MLServerError
 from mlserver.types import (
     InferenceRequest,
     InferenceResponse,
 )
-from mlserver.codecs import (
-    StringCodec,
+from mlserver.codecs import NumpyCodec, NumpyRequestCodec
+from {{ cookiecutter.repo_name }}.common import (
+    {{ cookiecutter.class_name }}Settings,
+    load_model_from_settings,
 )
-from mlserver_huggingface.common import (
-    HuggingFaceSettings,
-    parse_parameters_from_env,
-    InvalidTranformerInitialisation,
-    load_pipeline_from_settings,
-)
-from mlserver_huggingface.codecs import MultiStringRequestCodec
-from transformers.pipelines import SUPPORTED_TASKS
-from optimum.pipelines import SUPPORTED_TASKS as SUPPORTED_OPTIMUM_TASKS
 
 
-class HuggingFaceRuntime(MLModel):
+class {{ cookiecutter.class_name }}(MLModel):
     """Runtime class for specific Huggingface models"""
 
     def __init__(self, settings: ModelSettings):
-        env_params = parse_parameters_from_env()
-        if not env_params and (
-            not settings.parameters or not settings.parameters.extra
-        ):
-            raise InvalidTranformerInitialisation(
-                500,
-                "Settings parameters not provided via config file nor env variables",
-            )
 
-        extra = env_params or settings.parameters.extra  # type: ignore
-        self.hf_settings = HuggingFaceSettings(**extra)  # type: ignore
-
-        if self.hf_settings.task not in SUPPORTED_TASKS:
-            raise InvalidTranformerInitialisation(
-                500,
-                (
-                    f"Invalid transformer task: {self.hf_settings.task}."
-                    f" Available tasks: {SUPPORTED_TASKS.keys()}"
-                ),
-            )
-
-        if self.hf_settings.optimum_model:
-            if self.hf_settings.task not in SUPPORTED_OPTIMUM_TASKS:
-                raise InvalidTranformerInitialisation(
-                    500,
-                    (
-                        f"Invalid transformer task for "
-                        f"OPTIMUM model: {self.hf_settings.task}. "
-                        f"Supported Optimum tasks: {SUPPORTED_OPTIMUM_TASKS.keys()}"
-                    ),
-                )
+        self._extra_settings = {{ cookiecutter.class_name }}Settings(**settings.parameters.extra)  # type: ignore
 
         super().__init__(settings)
 
     async def load(self) -> bool:
-        # Loading & caching pipeline in asyncio loop to avoid blocking
-        await asyncio.get_running_loop().run_in_executor(
-            None, load_pipeline_from_settings, self.hf_settings
-        )
-        # Now we load the cached model which should not block asyncio
-        self._model = load_pipeline_from_settings(self.hf_settings)
+        # Simple showcase reading a lambda as string either from file or 
+        try:
+            model_uri = await get_model_uri(self._settings)
+            with open(model_uri, "r") as f:
+                self._model = eval(f.read())
+        except InvalidModelURI:
+            self._model = eval(self._extra_settings.lambda_value)
+
+        if not callable(self._model):
+            raise MLServerError("Invalid lambda value provided", status.HTTP_500_INTERNAL_SERVER_ERROR)
+
         self.ready = True
         return self.ready
 
     async def predict(self, payload: InferenceRequest) -> InferenceResponse:
         """
-        TODO
+        Prediction request
         """
+        # For more advanced request decoding see MLServer codecs documentation
+        model_input = NumpyRequestCodec.decode(payload)
 
-        # TODO: convert and validate?
-        kwargs = self.decode_request(payload, default_codec=MultiStringRequestCodec)
+        model_output = self._model(model_input)
 
-        args = []
-        if "args" in kwargs:
-            args = kwargs["args"]
-            del kwargs["args"]
-        prediction = self._model(*args, **kwargs)
-
-        # TODO: Convert hf output to v2 protocol, for now we use to_json
-        prediction_encoded = StringCodec.encode(
-            payload=[json.dumps(prediction)], name="output"
-        )
+        encoded_output = self.encode_response(model_output, default_codec=NumpyCodec)
 
         return InferenceResponse(
             model_name=self.name,
             model_version=self.version,
-            outputs=[prediction_encoded],
+            outputs=[encoded_output],
         )
